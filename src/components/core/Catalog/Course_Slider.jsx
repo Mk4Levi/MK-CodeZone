69a2ad76a766ba6afe706d744580f307d57dba43
Manthan <manthansoul@gmail.com>
2024-08-06 17:03:32 +0530

    Create components for Catalog

---
+++ src/components/core/Catalog/Course_Slider.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import { Autoplay, Navigation } from "swiper/modules";
+
+import Course_Card from "./Course_Card";
+
+function Course_Slider({ Courses }) {
+  return (
+    <>
+      {Courses?.length ? (
+        <Swiper
+          spaceBetween={30}
+          loop={true}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          // navigation={true}
+          modules={[Autoplay, Navigation]}
+          breakpoints={{
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+          className="max-h-[30rem]"
+        >
+          {Courses?.map((course, i) => (
+            <SwiperSlide key={i}>
+              <Course_Card course={course} Height={"h-[250px]"} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <p className="text-xl text-richblack-5">No Course Found</p>
+      )}
+    </>
+  );
+}
+
+export default Course_Slider;
