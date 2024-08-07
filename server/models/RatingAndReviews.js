70a0e3e85ad449a7bcbbf17ead2f95abddd800d5
Manthan <manthansoul@gmail.com>
2024-08-07 18:46:54 +0530

    Create RatingAndReviews.js

---
+++ server/models/RatingAndReviews.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const ratingAndReviewSchema = new mongoose.Schema({
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "user",
+	},
+	rating: {
+		type: Number,
+		required: true,
+	},
+	review: {
+		type: String,
+		required: true,
+	},
+	course: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "Course",
+		index: true,
+	},
+});
+
+// Export the RatingAndReview model
+module.exports = mongoose.model("RatingAndReview", ratingAndReviewSchema);
